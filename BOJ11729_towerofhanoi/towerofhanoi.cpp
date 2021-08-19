387a415e1c1cc10279c0f0cccb1a9d0496859f43
wjoh0315 <65387631+wjoh0315@users.noreply.github.com>
2021-08-19 18:19:00 +0900

    BOJ11729_FirstSolve

---
+++ BOJ11729_towerofhanoi/towerofhanoi.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void hanoi(int n, int from, int remain, int target)
+{
+    if (n <= 0)
+        return;
+    else if (n == 1)
+        cout << from << ' ' << target << '\n';
+    else
+    {
+        hanoi(n - 1, from, target, remain);
+        hanoi(1, from, remain, target);
+        hanoi(n - 1, remain, from, target);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int in;
+    cin >> in;
+    
+    cout << (1 << in) - 1 << '\n';
+    hanoi(in, 1, 2, 3);
+}
