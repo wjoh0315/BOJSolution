45385ec3a4aae3a38c1e5265f9c2d8156f5363f9
wjoh0315 <65387631+wjoh0315@users.noreply.github.com>
2021-08-24 20:01:07 +0900

    BOJ15649_FirstSolve

---
+++ BOJ15649_NandM1/NandM1.cpp
@@ -0,0 +1,49 @@
+//https://www.acmicpc.net/problem/15649
+#include <iostream>
+using namespace std;
+
+void printsequencef(int n, int m, int cur, int current[8], int pre[8])
+{
+    if (m <= 0)
+    {
+        for (int i=0; i < 8; i++)
+        {
+            if (current[i] == 0)
+                break;
+            cout << current[i] << ' ';
+        }
+
+        cout << '\n';
+    }
+    else 
+    {
+        for (int i=0; i < n; i++)
+        {
+            if (pre[i] == 0)
+            {
+                pre[i] = 1;
+                current[cur] = i + 1;
+                printsequencef(n, m - 1, cur + 1, current, pre);
+                pre[i] = 0;
+            }
+        }   
+    }
+}
+
+void printsequence(int n, int m)
+{
+    int current[8] = { 0, };
+    int pre[8] = { 0, };
+    printsequencef(n, m, 0, current, pre);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    printsequence(n, m);
+}
