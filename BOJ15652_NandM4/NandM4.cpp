c02c8b63341bfa5f56950c6b2b4573276e3afcfc
wjoh0315 <65387631+wjoh0315@users.noreply.github.com>
2021-08-25 10:35:07 +0900

    BOJ15652_FirstSolve

--- BOJ15652_NandM4/NandM4.cpp
+++ BOJ15652_NandM4/NandM4.cpp
@@ -1,12 +1,12 @@
-//https://www.acmicpc.net/problem/15651
+//https://www.acmicpc.net/problem/15652
 #include <iostream>
 using namespace std;
 
-void printsequencef(int n, int m, int cur, int current[7])
+void printsequencef(int n, int m, int cur, int current[8])
 {
     if (m <= 0)
     {
-        for (int i=0; i < 7; i++)
+        for (int i=0; i < 8; i++)
         {
             if (current[i] == 0)
                 break;
@@ -19,6 +19,11 @@ void printsequencef(int n, int m, int cur, int current[7])
     {
         for (int i=0; i < n; i++)
         {
+            if (cur - 1 >= 0)
+            {
+                if (current[cur - 1] > i + 1)
+                    continue;
+            }
             current[cur] = i + 1;
             printsequencef(n, m - 1, cur + 1, current);
         }   
@@ -27,7 +32,7 @@ void printsequencef(int n, int m, int cur, int current[7])
 
 void printsequence(int n, int m)
 {
-    int current[7] = { 0, };
+    int current[8] = { 0, };
     printsequencef(n, m, 0, current);
 }
 
