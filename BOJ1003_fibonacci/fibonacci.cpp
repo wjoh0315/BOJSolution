1e698367ddf10ac340d4cd866f168e3d013b054e
wjoh0315 <65387631+wjoh0315@users.noreply.github.com>
2021-08-30 20:34:33 +0900

    BOJ1003_FirstSolve

--- BOJ1003_fibonacci/fibonacci.cpp
+++ BOJ1003_fibonacci/fibonacci.cpp
@@ -2,3 +2,52 @@
 #include <iostream>
 using namespace std;
 
+void fibonacci(int current, int *count0, int *count1, int temp[41][2])
+{
+    if (current == 0)
+    {
+        *count0 += 1;
+        return;
+    }
+    else if (current == 1)
+    {
+        *count1 += 1;
+        return;
+    }
+
+    if (temp[current][0] != 0 && temp[current][1] != 0)
+    {
+        *count0 += temp[current][0];
+        *count1 += temp[current][1];
+        return;
+    }
+        
+    fibonacci(current - 1, count0, count1, temp);
+    fibonacci(current - 2, count0, count1, temp);
+
+    temp[current][0] = *count0;
+    temp[current][1] = *count1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    int n;
+    int count0 = 0;
+    int count1 = 0;
+    int temp[41][2] = { { 0, } };
+        
+    for (int i=0; i < t; i++)
+    {
+        cin >> n;
+        fibonacci(n, &count0, &count1, temp);
+        cout << count0 << ' ' << count1 << '\n';
+        count0 = 0;
+        count1 = 0;
+    }
+}
