4ac710ff0c35efea60c2d7e5ca83d6e78a300a46
wjoh0315 <65387631+wjoh0315@users.noreply.github.com>
2021-09-01 18:51:05 +0900

    BOJ1904_FirstSolve

---
+++ BOJ1904_01tile/01tile.cpp
@@ -0,0 +1,30 @@
+//https://www.acmicpc.net/problem/1904
+#include <iostream>
+using namespace std;
+
+int tile(int current, int temp[2])
+{
+    if (current == 1)
+        return 1;
+    else if (current == 2)
+        return 2;
+
+    int sum = 0;
+    for (int i=0; i < current - 2; i++)
+    {
+        sum = temp[0] + temp[1];
+        temp[0] = temp[1];
+        temp[1] = sum % 15746;
+    }
+    
+    return temp[1];
+}
+
+int main()
+{
+    int in;
+    cin >> in;
+
+    int temp[2] = { 1, 2 };
+    cout << tile(in, temp) << '\n';
+}
